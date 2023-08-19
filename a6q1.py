5e38b5d4e4f95616c2a3ae75689ea4f03f825cc9
Harry6542 <ozc189@usak.ca>
2023-08-19 14:37:31 -0600

    defined initialRead_state():

--- a6q1.py
+++ a6q1.py
@@ -2,3 +2,8 @@
 #NSID-ozc189
 #Student Number-11358887
 #Instructor-Lauressa Stilling
+def initialRead_state(filename):
+    with open(filename, 'r') as file:
+        lines = file.readlines()
+    initialState = [list(line.strip()) for line in lines]
+    return initialState
