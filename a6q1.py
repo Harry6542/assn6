3cc0992a9cb003fe04526d2a680e9748f6293827
Harry6542 <ozc189@usak.ca>
2023-08-19 14:39:37 -0600

    defined neighboursCounting(grid_display, i, j):

--- a6q1.py
+++ a6q1.py
@@ -7,3 +7,12 @@ def initialRead_state(filename):
         lines = file.readlines()
     initialState = [list(line.strip()) for line in lines]
     return initialState
+def neighboursCounting(grid_display, i, j):
+    neighbours = []
+    rows, cols = len(grid_display), len(grid_display[0])
+
+    directions = [(-1, 0), (1, 0), (0, -1), (0, 1), (-1, -1), (-1, 1), (1, -1), (1, 1)]
+    for dx, dy in directions:
+        if 0 <= i + dx < rows and 0 <= j + dy < cols:
+            neighbours.append(grid_display[i + dx][j + dy])
+    return neighbours
